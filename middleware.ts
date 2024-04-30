6618543244a22f3a5bd8bcf4b96cd8bee88de6b7
unknown <mohamed.hussein151515@gmail.com>
2024-04-30 14:55:11 +0300

    altered the database timezone to be Europe/Rome and added the same modification to the middleware in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,16 @@
+import { format } from "date-fns";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
   if (req.nextUrl.pathname.startsWith("/restaurant")) {
+    const romeTimeZone = "Europe/Rome";
+    const currentDate = new Date().toLocaleString("en-US", {
+      timeZone: romeTimeZone,
+    });
+    const formattedDate = format(currentDate, "yyyy-MM-dd");
+
     if (!req.nextUrl.searchParams.has("date")) {
-      req.nextUrl.searchParams.set(
-        "date",
-        new Date().toISOString().split("T")[0]
-      );
+      req.nextUrl.searchParams.set("date", formattedDate);
       return NextResponse.redirect(req.nextUrl);
     }
 
@@ -16,10 +20,7 @@ export async function middleware(req: NextRequest) {
     now.setHours(0, 0, 0, 0);
 
     if (date < now) {
-      req.nextUrl.searchParams.set(
-        "date",
-        new Date().toISOString().split("T")[0]
-      );
+      req.nextUrl.searchParams.set("date", formattedDate);
       return NextResponse.redirect(req.nextUrl);
     }
 
